a68a24ff3a018093149ee2ccb6c00bdae8a761d1
MkMuhammetKaradag <mk.muhammetkaradag@gmail.com>
2025-05-01 13:33:03 +0300

    fix(user): re-add initializer removed by mistake

---
+++ user/internal/initializer/redis.go
@@ -0,0 +1,23 @@
+package initializer
+
+import (
+	"log"
+	"socialmedia/user/infra/redisrepo"
+	"socialmedia/user/pkg/config"
+)
+
+func InitRedis(appConfig config.Config) *redisrepo.RedisRepository {
+	redisRepo, err := redisrepo.NewRedisRepository(appConfig.Redis.RedisURL, appConfig.Redis.Password, appConfig.Redis.DB)
+	if err != nil {
+		log.Fatalf("Redis connection failed: %v", err)
+	}
+	return redisRepo
+}
+
+func InitUserRedis(appConfig config.Config) *redisrepo.UserRedisRepository {
+	redisRepo, err := redisrepo.NewUserRedisRepository(appConfig.Redis.RedisURL, appConfig.Redis.Password, appConfig.Redis.UserDB)
+	if err != nil {
+		log.Fatalf("Redis connection failed: %v", err)
+	}
+	return redisRepo
+}
